3b8cb325cf2bdf0533927b360d5ac692bf716177
TestLeafPages <sarath.muthuswamy@testleaf.com>
2020-05-07 14:21:22 +0530

    Update FileDownload.java

--- FileDownload.java
+++ FileDownload.java
@@ -10,7 +10,7 @@ public class FileDownload {
 
 	public static void main(String[] args) {
 		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-		chromePrefs.put("profile.default_content_settings.popups", 0);
+		//chromePrefs.put("profile.default_content_settings.popups", 0);
 		chromePrefs.put("download.default_directory", "D:\\downloads");
 		ChromeOptions options = new ChromeOptions();
 		options.setExperimentalOption("prefs", chromePrefs);
